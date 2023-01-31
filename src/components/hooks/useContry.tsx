afccb5bf4b6e83ce686ea0eba98db823da6dd05b
BobadillaLeonRodrigo <al222110815@gmail.com>
2023-01-30 19:46:52 -0600

    Cambio '/contry' por '/users'

--- src/components/hooks/useContry.tsx
+++ src/components/hooks/useContry.tsx
@@ -2,12 +2,13 @@ import {useState, useRef, useEffect} from 'react'
 import { IDatum, IReqRestApiContry } from '../interfaces/IReqRestApiContry';
 import ReqRestApi from '../Api/ReqRestApi';
 const DEFAULT_PAGE = 1;
+//Cambio de '/contry' a '/users'
 export const useContry = () => {
     const [contrys, setContrys] = useState<IDatum[]>([]);
     const currentPage = useRef(DEFAULT_PAGE);
         const getData = async () => {
             const response = await ReqRestApi.get<IReqRestApiContry>(
-                '/contry', { params: { page: currentPage.current } }).then(
+                '/users', { params: { page: currentPage.current } }).then(
                     ({ data }) => data
                 ).catch(console.log);
                     //console.log(response);
